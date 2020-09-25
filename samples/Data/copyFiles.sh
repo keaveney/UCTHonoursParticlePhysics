fbbfbd58e9b06382aaf5f54e708a72f9072e001f
James Michael Keaveney <james.keaveney@desy.de>
2020-09-25 09:45:11 +0200

    first commit of graviton analysis adapted from official ATLAS OpenData examples

---
+++ samples/Data/copyFiles.sh
@@ -0,0 +1,4 @@
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_A.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_B.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_C.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_D.4lep.root
---
+++ samples/MC/copyFiles.sh
@@ -0,0 +1,8 @@
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361106.Zee.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361107.Zmumu.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410000.ttbar.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410155.ttW.4lep.root	
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410218.ttee.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410219.ttmumu.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_363490.llll.4lep.root
+curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_307434.RS_G_ZZ_llll_c10_m0500.4lep.root   
