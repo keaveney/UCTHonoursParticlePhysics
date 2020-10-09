97bd1489b5f3b4f7824c6ae5ef953149bc55f97c
James Michael Keaveney <james.keaveney@desy.de>
2020-10-09 10:55:20 +0200

    renamed graviton nb

--- samples/Data/copyFiles.sh
+++ samples/Data/copyFiles.sh
@@ -1,4 +1,14 @@
+#running this file: 'source copyFiles.sh'
+#will download the files for the graviton analysis
+#if you are running on MacOS, uncomment the four "curl ..." commands.
+#if you are running on linux, instead uncomment the wget.
+#If neither works you can always paste the links in your browser and download manually
+
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_A.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_B.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_C.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_D.4lep.root
+
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_A.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_B.4lep.root                                                         
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_C.4lep.root                                                         #wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/Data/data_D.4lep.root                                                          
--- samples/MC/copyFiles.sh
+++ samples/MC/copyFiles.sh
@@ -1,3 +1,7 @@
+#running this file: 'source copyFiles.sh'                                                                                                                                      
+#will download the files for the graviton analysis                                                                                                                              
+#if you are running on MacOS, uncomment the four "curl ..." commands.                                                                                                          #if you are running on linux, instead uncomment the wget.                                                                                                                       
+#If neither works you can always paste the links in your browser and download manually 
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361106.Zee.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361107.Zmumu.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410000.ttbar.4lep.root
@@ -6,3 +10,13 @@ curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/m
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410219.ttmumu.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_363490.llll.4lep.root
 curl -O https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_307434.RS_G_ZZ_llll_c10_m0500.4lep.root   
+
+
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361106.Zee.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_361107.Zmumu.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410000.ttbar.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410155.ttW.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410218.ttee.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_410219.ttmumu.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_363490.llll.4lep.root
+#wget --no-check-certificate https://atlas-opendata.web.cern.ch/atlas-opendata/samples/2020/4lep/MC/mc_307434.RS_G_ZZ_llll_c10_m0500.4lep.root
